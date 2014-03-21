6624950a136d6de24467f820400f0b0b5c2011c2
Luis Armendariz <luis.armendariz@gmail.com>
2014-03-21 12:00:43 -0700

    Added formula for fuego

---
+++ fuego-dev.rb
@@ -0,0 +1,18 @@
+require "formula"
+
+class FuegoDev < Formula
+  homepage "http://fuego.sourceforge.net/"
+  url "http://svn.code.sf.net/p/fuego/code/trunk"
+  version "HEAD"
+
+  depends_on 'boost'
+
+  def install
+    system "autoreconf", "-fi"
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+end
