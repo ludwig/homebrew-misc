d37680a82826e334c140dba2a3031e8e153470db
Luis Armendariz <luis.armendariz@openx.com>
2015-06-02 13:15:30 -0700

    Added formula for Winpdb 1.4.8

---
+++ winpdb-dev.rb
@@ -0,0 +1,23 @@
+require 'formula'
+
+# Winpdb homebrew formula
+# https://github.com/Homebrew/homebrew/pull/5772
+# https://github.com/Homebrew/homebrew/pull/5772/files
+
+class WinpdbDev < Formula
+  url 'http://winpdb.googlecode.com/files/winpdb-1.4.8.zip'
+  homepage 'http://winpdb.org/'
+  md5 '0860b82ac9bf3975042aa13018d38b25'
+
+  depends_on 'wxpython'
+
+  def install
+    # Find the arch for the Python we are building against.
+    # We remove 'ppc' support, so we pass Intel-optimized CFLAGS.
+    archs = archs_for_command("python")
+    archs.remove_ppc!
+    ENV['ARCHFLAGS'] = archs.as_arch_flags
+
+    system "python", "setup.py", "install", "--prefix=#{prefix}"
+  end
+end
